ddab7224412bf468a58d50ee2260ad2336406e84
kthfan <58523053+kthfan@users.noreply.github.com>
2024-07-10 14:33:41 +0800

    Add files via upload

--- resume/index.js
+++ resume/index.js
@@ -5,25 +5,25 @@ var iFrame = document.getElementById("iframe");
 var chineseBn = document.getElementById("chinese-bn");
 var englishBn = document.getElementById("english-bn");
 var downloadPdfBn = document.getElementById("download-pdf-bn");
-var langMode = "cn";
+var langMode = "zh-tw";
 
 
 function refreshCN(){
-    langMode = "cn";
+    langMode = "zh-tw";
     iFrame.height = 3300;
     
-    downloadPdfBn.href = "resume-cn.pdf";
+    downloadPdfBn.href = "resume-zh-tw.pdf";
 
     chineseBn.classList.add("selected-lang-bn");
     englishBn.classList.remove("selected-lang-bn");
 }
 
 function refreshEN(){
-    langMode = "en";
-    iFrame.src = "content_en.html";
+    langMode = "en-us";
+    iFrame.src = "content-en-us.html";
     iFrame.height = 4000;
 
-    downloadPdfBn.href = "resume-en.pdf";
+    downloadPdfBn.href = "resume-en-us.pdf";
 
     englishBn.classList.add("selected-lang-bn");
     chineseBn.classList.remove("selected-lang-bn");
@@ -34,14 +34,14 @@ chineseBn.addEventListener("load", refreshCN);
 englishBn.addEventListener("load", refreshEN);
 
 chineseBn.addEventListener("click", () => {
-    iFrame.src = "content_cn.html";
+    iFrame.src = "content-zh-tw.html";
     refreshCN();
 });
 
 englishBn.addEventListener("click", () => {
-    iFrame.src = "content_en.html";
+    iFrame.src = "content-en-us.html";
     refreshEN();
 });
 
-iFrame.src = "content_cn.html";
+iFrame.src = "content-zh-tw.html";
 refreshCN();
